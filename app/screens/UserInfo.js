dac3b5dd21364186bef76a79ab6e9285710047ed
Jose <jmejiamu@gmail.com>
2021-03-12 23:56:01 -0800

    retrieving info

--- app/screens/UserInfo.js
+++ app/screens/UserInfo.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 
 const UserInfo = () => {
     const [userData, setUserData] = useState([]);
 
     const getUserData = async () => {
-        const response = await fetch('http://192.168.1.69:3006/userinfo');
-        const data = await response.json()
-        console.log(data);
-        setUserData(data);
+        try {
+            const response = await fetch('http://192.168.1.69:3006/userinfo');
+            const data = await response.json()
+            setUserData(data);
+
+        } catch (error) {
+            console.error(error.message);
+        }
+
+        getUserData();
+
     }
 
     useEffect(() => {
@@ -18,21 +25,41 @@ const UserInfo = () => {
     }, [])
 
     return (
-        <View>
-            {
-                userData.map((item, index) => {
-                    return (
-                        <View key={index}>
-                            <Text>{item.name}</Text>
-                            <Text>{item.fav_color}</Text>
+        <FlatList
+            data={userData}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => {
+                return (
+
+                    <View style={{ flex: 1 }} >
+                        <View style={styles.card}>
+                            <Text>Name:{item.name}</Text>
                             <Text>{item.email}</Text>
+                            <Text>{item.fav_color}</Text>
                         </View>
-                    )
-                })
-            }
-            <Text>User data</Text>
-        </View>
+                    </View>
+                )
+            }}
+        />
+
     )
 }
 
 export default UserInfo
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: 'white',
+        margin: 10,
+        padding: 20,
+        flex: 1,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 6.27,
+        elevation: 10,
+    },
+})
